d71d804fcb07ae97b29f6ba760ed657eff4fd29b
aaravarora155 <aarav2607@gmail.com>
2026-01-12 17:52:34 +0000

    Commit

---
+++ PhysicsSim/src/main/java/frc/robot/commands/PhysicsCalc.java
@@ -0,0 +1,12 @@
+package frc.robot.commands;
+
+public class PhysicsCalc {
+    private static final double gravity = 9.8;
+    private static final double velocity=30; //(m/s)
+    private static final double height = 1.8288; //(m)
+    private static final double distance = 1.5; //(m)
+
+    public static void main(String[] args){
+        System.out.println("Welcome to Physics Calculator");
+    }
+}
--- src/PhysicCalc.java
+++
@@ -1,5 +0,0 @@
-public class PhysicCalc {
-    public static void main(String[] args){
-        System.out.println("Welcome to Physics Calculator");
-    }
-}
