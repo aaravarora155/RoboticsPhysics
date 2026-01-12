dc18538aeeef3d6e988b1d817f4e651aa48a144e
aaravarora155 <aarav2607@gmail.com>
2026-01-12 21:56:49 +0000

    Commit

--- PhysicsSim/src/main/java/frc/robot/commands/PhysicsCalc.java
+++ PhysicsSim/src/main/java/frc/robot/commands/PhysicsCalc.java
@@ -1,12 +1,53 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+
+import java.util.Optional;
+
 public class PhysicsCalc {
+    //Unused Constants
     private static final double gravity = 9.8;
     private static final double velocity=30; //(m/s)
     private static final double height = 1.8288; //(m)
-    private static final double distance = 1.5; //(m)
+
+    //Poses
+    private static final Translation2d robotPose = new Translation2d();
+    private static Translation2d hubPose;
+
+    //Distance Vector Calculation
+    private static Translation2d distance2d;
+
+    //Distances
+    private static double distanceX;
+    private static double distanceY;
+    private static double distance;
+
+    public static void calc(Translation2d robotPose, DriverStation.Alliance alliance) {
+
+        //Set hub pose based on alliance
+        if (alliance==DriverStation.Alliance.Red){
+            hubPose = new Translation2d(11.9,4.03);
+        }
+        else{
+            hubPose = new Translation2d(4.63,4.03);
+        }
+
+        //Calculate the Straight line distance in (m) to the hub
+        distance2d = hubPose.minus(robotPose);
+
+        distanceX = distance2d.getX();
+        distanceY = distance2d.getY();
+
+        distance = (double) Math.round(Math.hypot(distanceX,distanceY)*100)/100;
+    }
+
 
     public static void main(String[] args){
+        PhysicsCalc.calc(robotPose, DriverStation.Alliance.Red);
         System.out.println("Welcome to Physics Calculator");
+        System.out.println("X:"+distanceX);
+        System.out.println("Y:"+distanceY);
+        System.out.println("Distance:"+distance);
     }
 }
