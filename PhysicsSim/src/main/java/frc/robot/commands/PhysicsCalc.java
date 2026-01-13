eb20017a9434a1287999c7325c1561e9371c4e54
aaravarora155 <aarav2607@gmail.com>
2026-01-13 17:01:51 +0000

    Created lookup table template

--- PhysicsSim/src/main/java/frc/robot/commands/PhysicsCalc.java
+++ PhysicsSim/src/main/java/frc/robot/commands/PhysicsCalc.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PhysicsCalc {
+    private static Map<Double,Double> lookupTable = new  HashMap<Double, Double>();
+
     public static double calc(Translation2d robotPose, DriverStation.Alliance alliance) {
         //Pose
         Translation2d hubPose;
@@ -36,24 +38,91 @@ public class PhysicsCalc {
         distanceX = distance2d.getX();
         distanceY = distance2d.getY();
 
-        distance = (double) Math.round(Math.hypot(distanceX,distanceY)*100)/100;
+        distance = (double) Math.round(Math.hypot(distanceX,distanceY)*10)/10;
 
-        theta = runRegression(distance);
+        theta = find(distance);
         return theta;
     }
-//    public static void lookupTable(){
-//        /*
-//        * Key: Distance From Hub (double)
-//        * Value: Angle (double) (0-90)
-//        * */
-//        lookupTable.put(distance,runRegression(distance));
-//
-//    }
-    public static Double runRegression(double distance){
-        //Ensure that distance is positive
-        distance=Math.abs(distance);
-        return (double) 0.0; //Run the regression on the distance to get an output of theta and return it
+    public static void lookupTable(){
+        /*
+        * Key: Distance From Hub (double)
+        * Value: Angle (double) (0-90)
+        * Keys are spaced out by increments of 0.1
+        *
+        * Closest point is 0m away
+        * Farthest point is 6.2m
+        * */
+
+        lookupTable.put(0.0, 0.0);
+        lookupTable.put(0.1, 0.0);
+        lookupTable.put(0.2, 0.0);
+        lookupTable.put(0.3, 0.0);
+        lookupTable.put(0.4, 0.0);
+        lookupTable.put(0.5, 0.0);
+        lookupTable.put(0.6, 0.0);
+        lookupTable.put(0.7, 0.0);
+        lookupTable.put(0.8, 0.0);
+        lookupTable.put(0.9, 0.0);
+        lookupTable.put(1.0, 0.0);
+        lookupTable.put(1.1, 0.0);
+        lookupTable.put(1.2, 0.0);
+        lookupTable.put(1.3, 0.0);
+        lookupTable.put(1.4, 0.0);
+        lookupTable.put(1.5, 0.0);
+        lookupTable.put(1.6, 0.0);
+        lookupTable.put(1.7, 0.0);
+        lookupTable.put(1.8, 0.0);
+        lookupTable.put(1.9, 0.0);
+        lookupTable.put(2.0, 0.0);
+        lookupTable.put(2.1, 0.0);
+        lookupTable.put(2.2, 0.0);
+        lookupTable.put(2.3, 0.0);
+        lookupTable.put(2.4, 0.0);
+        lookupTable.put(2.5, 0.0);
+        lookupTable.put(2.6, 0.0);
+        lookupTable.put(2.7, 0.0);
+        lookupTable.put(2.8, 0.0);
+        lookupTable.put(2.9, 0.0);
+        lookupTable.put(3.0, 0.0);
+        lookupTable.put(3.1, 0.0);
+        lookupTable.put(3.2, 0.0);
+        lookupTable.put(3.3, 0.0);
+        lookupTable.put(3.4, 0.0);
+        lookupTable.put(3.5, 0.0);
+        lookupTable.put(3.6, 0.0);
+        lookupTable.put(3.7, 0.0);
+        lookupTable.put(3.8, 0.0);
+        lookupTable.put(3.9, 0.0);
+        lookupTable.put(4.0, 0.0);
+        lookupTable.put(4.1, 0.0);
+        lookupTable.put(4.2, 0.0);
+        lookupTable.put(4.3, 0.0);
+        lookupTable.put(4.4, 0.0);
+        lookupTable.put(4.5, 0.0);
+        lookupTable.put(4.6, 0.0);
+        lookupTable.put(4.7, 0.0);
+        lookupTable.put(4.8, 0.0);
+        lookupTable.put(4.9, 0.0);
+        lookupTable.put(5.0, 0.0);
+        lookupTable.put(5.1, 0.0);
+        lookupTable.put(5.2, 0.0);
+        lookupTable.put(5.3, 0.0);
+        lookupTable.put(5.4, 0.0);
+        lookupTable.put(5.5, 0.0);
+        lookupTable.put(5.6, 0.0);
+        lookupTable.put(5.7, 0.0);
+        lookupTable.put(5.8, 0.0);
+        lookupTable.put(5.9, 0.0);
+        lookupTable.put(6.0, 0.0);
+        lookupTable.put(6.1, 0.0);
+        lookupTable.put(6.2, 0.0);
+
     }
+    public static double find(double distance){
+        //
+        return 0.0;
+    }
+
 
     public static void main(String[] args){
         double theta = PhysicsCalc.calc(new Translation2d(), DriverStation.Alliance.Red);
